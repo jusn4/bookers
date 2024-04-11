9a7e15bf3faa75dcb4bab71277c2efa7e52a583b
jusn4 <164776044+jusn4@users.noreply.github.com>
2024-04-11 12:28:03 +0000

    [Update]routes,index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/show'
+  get 'books/edit'
   get '/' => 'homes#top', as:'root'
-  resources :books
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
