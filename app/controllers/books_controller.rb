a58bf590d41861131d009c8b2f22dc14766bd6f4
jusn4 <164776044+jusn4@users.noreply.github.com>
2024-04-12 09:59:31 +0000

    [Update]諸機能の実装、レイアウトの整理

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      redirect_to '/books'
+    end
   end
 
   def index
@@ -34,7 +37,8 @@ class BooksController < ApplicationController
   end
 
   private
+
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
 end
