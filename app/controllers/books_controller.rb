77f948310025bd220839c0065d136be802768995
jusn4 <164776044+jusn4@users.noreply.github.com>
2024-04-13 02:18:57 +0000

    [Add]Flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was succesfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -25,6 +26,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was succesfully updated."
       redirect_to book_path(@book.id)
     else
       render :edit
